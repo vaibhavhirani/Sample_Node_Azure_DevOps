42964aaedffa0f74662391b2ecca4da30692ca73
Vaibhav Not Root <norma@LAPTOP-R39SKDCC.localdomain>
2022-05-24 22:23:44 +0530

    ADded index

--- app/index.js
+++ app/index.js
@@ -6,29 +6,3 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('Listening to Port 8080');
 });
-
-class Factorial {
-  constructor(){
-      
-  };
-
-  factorialShort(n) {
-      if (n < 0) {
-          throw new Error(`Negative number!`);
-      }
-      return (n !== 1) ? n * this.factorialShort(n - 1) : 1;
-  };
-
-  factorialSimple(n) {
-      if (n < 0) {
-          throw new Error(`Negative number!`);
-      }
-      if (n !== 1) {
-          return n * this.factorialShort(n-1);
-      } else {
-          return 1;
-      }
-  };
-};
-
-module.exports = Factorial;
